9978f9451235204735644ca227f75a9bb3c88d5a
Mantasha Zubair <98265444+MantashaZubair@users.noreply.github.com>
2023-02-07 08:48:33 -0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -30,3 +30,43 @@ init();
 app.listen(8000 ,()=>{
     console.log(`Server Listen at localhost on 8000 port`)
 })
+
+
+
+
+// const express = require("express");
+// const app =express();
+// const cors = require("cors")
+// app.use(cors());
+// const storage = require("node-persist");
+// app.use(express.json());
+// const init = async () => { await storage.init({ dir: "./todolist" });
+// };
+// init();
+
+//  app.get('/todo' , async (req,res)=>{
+//    const itemLists = await storage.getItem("todolist");
+//       itemLists.forEach((itemList)=>{
+//         return itemList.todo
+//       })
+
+//     // res.status(200).json(await storage.values(itemLists))
+//     res.status(200).json({ result: itemLists });
+//  })
+
+
+//  app.post("/todo", async (req, res) => { 
+//      const alltodo = await storage.getItem("todolist"); 
+//      const todo =  req.body;
+//      todo.id = alltodo.length + 1; 
+//     await storage.setItem("todolist", [...alltodo, todo]); 
+//     res.status(200).json({ success: true });
+  
+//    });
+   
+   
+
+
+// app.listen(8000 ,()=>{
+//     console.log(`Server Listen at localhost on 8000 port`)
+// })
