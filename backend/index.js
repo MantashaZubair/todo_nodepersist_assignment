2ccf54922145c20e5e8b55a292e444c6a03843c2
Mantasha Zubair <98265444+MantashaZubair@users.noreply.github.com>
2023-02-07 09:12:03 -0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,13 @@ const init = async () => { await storage.init({ dir: "./todolist" });
 init();
 
  app.get('/todo' , async (req,res)=>{
-   const itemList = await storage.getItem("todolist");
-    // res.status(200).json(await storage.values(itemList))
-    res.status(200).json({ result: itemList });
+   const itemLists = await storage.getItem("todolist");
+      itemLists.forEach((itemList)=>{
+        return itemList.todo
+      })
+
+    // res.status(200).json(await storage.values(itemLists))
+    res.status(200).json({ result: itemLists });
  })
 
 
@@ -30,43 +34,3 @@ init();
 app.listen(8000 ,()=>{
     console.log(`Server Listen at localhost on 8000 port`)
 })
-
-
-
-
-// const express = require("express");
-// const app =express();
-// const cors = require("cors")
-// app.use(cors());
-// const storage = require("node-persist");
-// app.use(express.json());
-// const init = async () => { await storage.init({ dir: "./todolist" });
-// };
-// init();
-
-//  app.get('/todo' , async (req,res)=>{
-//    const itemLists = await storage.getItem("todolist");
-//       itemLists.forEach((itemList)=>{
-//         return itemList.todo
-//       })
-
-//     // res.status(200).json(await storage.values(itemLists))
-//     res.status(200).json({ result: itemLists });
-//  })
-
-
-//  app.post("/todo", async (req, res) => { 
-//      const alltodo = await storage.getItem("todolist"); 
-//      const todo =  req.body;
-//      todo.id = alltodo.length + 1; 
-//     await storage.setItem("todolist", [...alltodo, todo]); 
-//     res.status(200).json({ success: true });
-  
-//    });
-   
-   
-
-
-// app.listen(8000 ,()=>{
-//     console.log(`Server Listen at localhost on 8000 port`)
-// })
