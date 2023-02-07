972f8e792d144ca95f271fff681de8c79b0c4c7c
Mantasha Zubair <98265444+MantashaZubair@users.noreply.github.com>
2023-02-07 09:13:10 -0800

    Update default.js

--- frontend/src/default.js
+++ frontend/src/default.js
@@ -227,3 +227,38 @@ export default ToDoForm
 //   )
 // }
 // export default ToDoForm
+
+
+
+// const express = require("express");
+// const app =express();
+// const cors = require("cors")
+// app.use(cors());
+// const storage = require("node-persist");
+// app.use(express.json());
+// const init = async () => { await storage.init({ dir: "./todolist" });
+// };
+// init();
+
+//  app.get('/todo' , async (req,res)=>{
+//    const itemList = await storage.getItem("todolist");
+//     // res.status(200).json(await storage.values(itemList))
+//     res.status(200).json({ result: itemList });
+//  })
+
+
+//  app.post("/todo", async (req, res) => { 
+//      const alltodo = await storage.getItem("todolist"); 
+//      const todo =  req.body;
+//      todo.id = alltodo.length + 1; 
+//     await storage.setItem("todolist", [...alltodo, todo]); 
+//     res.status(200).json({ success: true });
+  
+//    });
+   
+   
+
+
+// app.listen(8000 ,()=>{
+//     console.log(`Server Listen at localhost on 8000 port`)
+// })
