5cc59894537b5047bbaa94657ade139b48c19572
Mantasha Zubair <98265444+MantashaZubair@users.noreply.github.com>
2023-02-22 05:41:15 -0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,40 @@
 const express = require("express");
-const app =express();
-const cors = require("cors")
+const app = express();
+const cors = require("cors");
+
 app.use(cors());
-const storage = require("node-persist");
 app.use(express.json());
-const init = async () => { await storage.init({ dir: "./todolist" });
+
+const storage = require("node-persist");
+const init = async () => {
+  await storage.init({ dir: "./todolist" });
 };
 init();
 
- app.get('/todo' , async (req,res)=>{
-   const itemLists = await storage.getItem("todolist");
-
-    // res.status(200).json(await storage.values(itemLists))
+//------get api-----//
+  app.get("/todo", async (req, res) => {
+    const itemLists = await storage.getItem("todolist");
     res.status(200).json({ result: itemLists });
- })
+  });
 
 
- app.post("/todo", async (req, res) => { 
-     const alltodo = await storage.getItem("todolist"); 
-     const todo =  req.body;
-     todo.id = alltodo.length + 1; 
-    await storage.setItem("todolist", [...alltodo, todo]); 
+//-----create api-----//
+  app.post("/todo", async (req, res) => {
+    const alltodo = await storage.getItem("todolist");
+    const todo = req.body;
+    todo.id = alltodo ? alltodo.length + 1 : 1;
+    const newTodo = alltodo ? [...alltodo, todo] : [todo];
+    await storage.setItem("todolist", newTodo);
     res.status(200).json({ success: true });
-  
-   });
+  });
 
-   app.delete("/todo", async(req,res)=>{
-     const deleteitem=await storage.setItem("todolist", []);
-     res.status(200).json({ result: deleteitem });
-   })
-   
-   
+//------delete api-----//
+  app.delete("/todo", async (req, res) => {
+    await storage.clear();
+    res.status(200).json({ success: true });
+  });
 
-
-app.listen(8000 ,()=>{
-    console.log(`Server Listen at localhost on 8000 port`)
-})
+  //listen
+  app.listen(8000, () => {
+    console.log(`Server Listen at localhost on 8000 port`);
+  });
